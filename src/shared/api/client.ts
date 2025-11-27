6d6b97924b5aa74bb105b17439a7ffd4b4e44346
Mihail4531 <borovpaziloj4@gmail.com>
2025-11-27 21:36:53 +0300

    teqe

--- src/shared/api/client.ts
+++ src/shared/api/client.ts
@@ -1,11 +1,11 @@
 import axios from "axios";
-// import { API_URL } from "@/shared/config"; 
+import { API_URL } from "@/shared/config"; 
 import { API_HEADER } from "./config";
 
-const HARD_URL = "https://gremlini.store/api";
+
 
 export const $axios = axios.create({
-  baseURL: HARD_URL,
+  baseURL: API_URL,
   headers: API_HEADER,
   withCredentials: true,
 });
