d269d1b521492fa09259a42f4eb5c8700b7a7263
Mihail4531 <borovpaziloj4@gmail.com>
2025-11-27 21:20:15 +0300

    teqe

--- src/shared/api/client.ts
+++ src/shared/api/client.ts
@@ -2,6 +2,8 @@ import axios from "axios";
 import { API_URL } from "@/shared/config"; 
 import { API_HEADER } from "./config";
 
+console.log("🚀 MY API URL:", API_URL); 
+
 export const $axios = axios.create({
   baseURL: API_URL,
   headers: API_HEADER,
