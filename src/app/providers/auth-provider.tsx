d0eeab5621acc059e29a1c14f4d45caa0e666dfc
Mihail4531 <borovpaziloj4@gmail.com>
2025-11-27 21:49:46 +0300

    teqe

--- src/app/providers/auth-provider.tsx
+++ src/app/providers/auth-provider.tsx
@@ -1,13 +1,18 @@
 "use client";
 
-import { useEffect } from "react";
+import { useEffect, useRef } from "react";
 import { useAuthStore } from "@/entities/session";
+
 export const AuthProvider = ({ children }: { children: React.ReactNode }) => {
   const fetchUser = useAuthStore((state) => state.fetchUser);
+  // Используем ref, чтобы избежать двойного вызова в React Strict Mode (в режиме разработки)
+  const mounted = useRef(false);
 
   useEffect(() => {
-    const timer = setTimeout(() => fetchUser());
-    return () => clearTimeout(timer);
+    if (!mounted.current) {
+      fetchUser();
+      mounted.current = true;
+    }
   }, [fetchUser]);
 
   return <>{children}</>;
