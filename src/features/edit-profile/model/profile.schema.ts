0434843d45f3e85cc7a7be0509be307fc854bf5f
Mihail4531 <borovpaziloj4@gmail.com>
2025-11-29 01:15:17 +0300

    three

--- src/features/edit-profile/model/profile.schema.ts
+++ src/features/edit-profile/model/profile.schema.ts
@@ -3,7 +3,7 @@ import { z } from "zod";
 export const ProfileSchema = z.object({
  name: z.string().trim().min(1, "Имя обязательно"), 
   bio: z.string().trim().optional(),
-  website: z.string().trim().url("Некорректный URL").optional().or(z.literal("")),
+  website: z.string().trim().max(255, "Ссылка слишком длинная (макс. 255 символов)").url("Некорректный URL").optional().or(z.literal("")) ,
   github_username: z.string().trim().optional(),
   skills: z.string().optional(),
 });
