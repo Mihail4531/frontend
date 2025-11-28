116e8e129baef1899ef459b88c3e3830344e3f77
Mihail4531 <borovpaziloj4@gmail.com>
2025-11-28 20:24:50 +0300

    teqe

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,16 @@
 import { Inter } from "next/font/google";
 import "@/app/styles/globals.css";
 import { AuthProvider } from "@/app/providers";
-
+import { Metadata } from "next"; 
 const inter = Inter({ subsets: ["latin"] });
 
-export const metadata = {
+export const metadata: Metadata = {
   title: "Dev Community",
   description: "Platform for developers",
+  // 👇 ЯВНО УКАЗЫВАЕМ ПУТЬ
+  icons: {
+    icon: '/favicon.ico', 
+  },
 };
 
 export default function RootLayout({
